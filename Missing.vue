ad914f7c1204a65cd9e99a4b573c02ae6ddbf2db
Helveeti <56016617+Helveeti@users.noreply.github.com>
2020-05-06 20:15:24 +0300

    Navbar added to Missing.

--- Missing.vue
+++ Missing.vue
@@ -1,10 +1,29 @@
 <template>
-    <h1>Page not found. 404</h1>
+    <div id="missing">
+        <!-- NAV -->
+        <b-container>
+            <b-row>
+                <b-col>
+                    <navbar />
+                </b-col>
+            </b-row>
+            <b-row>
+                <b-col>
+                    <router-view/>
+                </b-col>
+            </b-row>
+        </b-container>
+
+        <h1>Page not found. 404</h1>
+    </div>
 </template>
 
 <script>
+    import navbar from "@/components/navbar";
+
     export default {
-        name: "Missing"
+        name: "Missing",
+        components: {navbar}
     }
 </script>
 
